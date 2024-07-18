be063093a1dbaba17882df16f7a22138919673aa
diayaya1999@gmail.com <diayaya1999@gmail.com>
2024-07-18 07:29:15 +0000

    le dernier changement

--- src/components/tache.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import img from './components/assets/G'
-
-function tache() {
-  return (
-                                                                                                                                                                                                                    
-
-export default ta
